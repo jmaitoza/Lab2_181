1f8460658d7b46bcc02f0a3f6ac5d2ffd2a30d31
Joshua Maitoza <60358495+jmaitoza@users.noreply.github.com>
2021-09-14 21:02:18 -0400

    created ChangePassword methods, still need to create unit tests

--- src/cp/ChangePassword.java
+++ src/cp/ChangePassword.java
@@ -16,6 +16,64 @@ public class ChangePassword
         return true;
     }
 
+    static boolean hasMinLength(String pwd)
+    {
+
+        if(pwd.length() >= 6)
+        {
+            return false;
+        }
+
+        else
+        {
+            System.out.println("Password must have a minimum of 6 characters!");
+            return true;
+        }
+    }
+
+    /**
+     * uses Regular Expressions to check whether String pwd contains either a digit (0-9)
+     * or any other printable special characters
+     * if pwd does match then the method returns false as it has special characters and no error is found
+     */
+    public static boolean hasOnlyLetters(String pwd)
+    {
+        if (pwd.matches(".*[^a-zA-Z].*"))
+        {
+            return false;
+        }
+
+        else
+        {
+            System.out.println("Password must contain at least one character that is not a letter!");
+            return true;
+        }
+    }
+
+    public static boolean doPasswordsMatch(String pwd, String pwd2)
+    {
+        if (pwd.equals(pwd2))
+        {
+            return false; // foundError = false
+        }
+
+        else
+        {
+            System.out.println("Passwords must match!");
+            return true;
+        }
+    }
+
+//    public static boolean checkForErrors(String pwd, String pwd2)
+//    {
+//        boolean error1,error2,error3;
+//       error1 = hasMinLength(pwd);
+//       error2 = hasOnlyLetters(pwd);
+//       error3 = doPasswordsMatch(pwd, pwd2);
+//       return error1;
+
+//    }
+
     public static void main(String[] args)
     {
         Console cons = System.console();
@@ -36,18 +94,22 @@ public class ChangePassword
             String pwd = new String(cons.readPassword("       Password:  "));
             String pwd2 = new String(cons.readPassword("Verify password:  "));
 
-            foundError = false;
-
             // In the following section, please fill out code to meet requirements of the lab description
+//            checkForErrors(pwd,pwd2);
+            boolean error1, error2, error3;
+            error1 = hasMinLength(pwd);
+            error2 = hasOnlyLetters(pwd);
+            error3 = doPasswordsMatch(pwd, pwd2);
 
+            if ((error1 || error2 || error3))
+                foundError = true;
 
-
-
-
+            else
+                foundError = false;
 
 
             // If we make it this far, we have passed all of the tests.
-            foundError = false;
+            //foundError = false;
         } while (foundError);
 
 
---
+++ src/cp/ChangePasswordTest.java
@@ -0,0 +1,29 @@
+package cp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.desktop.SystemEventListener;
+import java.io.Console;
+import java.io.PrintWriter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChangePasswordTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+        Console testCons = System.console();
+    }
+
+    @Test
+
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+}
